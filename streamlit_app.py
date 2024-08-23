53133d7594e5fc04b88951d3fdd518412293878e
mariam2508 <mariamhossam243@gmail.com>
2024-08-23 19:10:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ f2=st.number_input('feature 2',min_value=1,max_value=10)
 f3=st.number_input('feature 3',min_value=1,max_value=100)
 
 
-with open('model.pkl','rb')as file:
+with open('model (1).pkl','rb')as file:
   model=pickle.load(file)
 res = model.predict([[f1,f2,f3]])  
 
