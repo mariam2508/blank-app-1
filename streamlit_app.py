e85c93f1a6e8a496e3d7917454552193aca8e5c0
mariam2508 <mariamhossam243@gmail.com>
2024-08-23 18:53:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,7 @@ st.title("🎈 Car co2 emssion")
 f1=st.number_input('feature 1',min_value=1,max_value=10)
 f2=st.number_input('feature 2',min_value=1,max_value=10)
 f3=st.number_input('feature 3',min_value=1,max_value=100)
-with open('model.pkl','rb')as file:
-  model=pickle.load(file)
+
 
 
 
